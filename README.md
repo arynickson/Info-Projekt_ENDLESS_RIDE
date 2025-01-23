c60ea761bcf2dcc58c82c3904ddeb986280d10d5
arynickson <arynickson1208@gmail.com>
2025-01-23 19:23:41 +0100

    Erstellung

---
+++ README.md
@@ -0,0 +1,6 @@
+# Info-Projekt_ENDLESS_RIDE
+ Blender Addons, sowie Geometry und- Shader-Nodes für mein CGI-Filmprojekt "THE ENDLESS RIDE"
+
+## Addons, Shader & Geometry-Nodes
+ - Addons liegen hier vor als .py Dateien
+ - Shader&Geometry-Nodes als Screenshots der sog. "Nodetrees"
---
+++ addon_code/pixel_scaling_addon/__init__.py
@@ -0,0 +1,61 @@
+bl_info = {
+    "name": "Pixel-Texture-Scaling-Calc",
+    "blender": (4, 0, 0),
+    "category": "Object",
+}
+
+import bpy
+import subprocess
+
+def auto_copy(txt):
+    cmd='echo '+str(txt).strip()+'|clip'
+    return subprocess.check_call(cmd, shell=True)
+
+class PixelScalingCalc(bpy.types.Panel):
+
+    bl_label = "Pixel-Scaling-Calc"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_category = "Pixel-Scaling-Calc"
+
+    @classmethod
+    def poll(cls, context):
+        return (context.object is not None)
+    
+    def draw(self, context):
+        layout = self.layout
+        scene = bpy.context.scene
+        
+        ### liste aller 3druler längen
+        seitenlaengen = list((s.points[0].co - s.points[-1].co).length for s in bpy.data.grease_pencils["Annotations"].layers["RulerData3D"].frames[0].strokes)
+
+        ### loop durch bereich layout um grid scale zu finden
+        for area in bpy.data.screens["Layout"].areas:
+            if area.type == 'VIEW_3D':
+                for space in area.spaces:
+                    if space.type == 'VIEW_3D':
+                        grid_scaling_var = space.overlay.grid_scale
+                        break
+        
+        ### einfach seitenlänge vom 3druler durch die grid size für seitenlänge in pixeln
+        calc_pixel_scaling = round(seitenlaengen[-1] // grid_scaling_var)
+        box = layout.box()
+        ### text set
+        box.label(text="Measured length in px: "+str(calc_pixel_scaling))
+        
+        ### berechneten scaling wert zu clipboard automatisch kopieren
+        auto_copy(calc_pixel_scaling)
+ 
+
+
+bpy.utils.register_class(PixelScalingCalc)
+
+
+def register():
+    print("Hello World")
+
+def unregister():
+    print("Tschau")
+
+if __name__ == "__main__":
+    register()
